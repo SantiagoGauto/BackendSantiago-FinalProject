ec64b0849afb0bffb890e3e470c4bc6fdf20b9d2
SantiagoGauto <santiago.delgauto@gmail.com>
2023-04-25 20:24:30 -0300

    dotenvx2

--- src/utils/twilio.js
+++ src/utils/twilio.js
@@ -1,5 +1,5 @@
 import twilio from "twilio";
-require('dotenv').config()
+require('dotenv').config({path:'./.env.example'});
 
 const client = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
